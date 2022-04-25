5983ed1721fad584599635b4549d960ea2340446
App Academy Student <email@email.com>
2022-04-25 14:19:31 -0400

    finished auth css

--- frontend/components/navbar/nav_belt.jsx
+++ frontend/components/navbar/nav_belt.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import GreetingContainer from "../greeting_container";
+import SearchBarContainer from "./search_bar_container";
 
 const NavBelt = () => (
     <div className="nav-belt">
-        <h1>Jamazon!</h1>
+        <h1>Jamazon Logo Placeholder</h1>
+        <SearchBarContainer />
         <GreetingContainer />
+        {/* <ShoppingCart /> */}
     </div>
 )
 
---
+++ frontend/components/navbar/search_bar_container.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+import { connect } from "react-redux";
+import SearchBar from "./search_bar";
+
+export default connect(null, null)(SearchBar)
