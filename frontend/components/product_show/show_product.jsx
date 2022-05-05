5e0d5e2d26a21fd6db9a41b2a3a53d7d1736c250
Jackson <jacksondooley24@gmail.com>
2022-05-05 15:41:43 -0400

    working on show css

--- frontend/components/product_show/show_product.jsx
+++ frontend/components/product_show/show_product.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Reviews from "./reviews";
 import { BsStarFill, BsStarHalf, BsStar } from "react-icons/bs"
+import { FaLock } from 'react-icons/fa'
 import ReviewsContainer from "./reviews_container";
 
 class ShowProduct extends React.Component {
@@ -52,29 +53,83 @@ class ShowProduct extends React.Component {
                         <h1>
                             {this.props.product?.name}
                         </h1>
-                        <div>
+                        <div className="show-product-rating">
                             <div className="stars">
                                 {this.ratingStars(this.props.product?.avg_rating)} 
                             </div>
-                            <div>
+                            <div className="show-product-ratings">
                                 {this.props.product?.review_count} ratings
                             </div>
                         </div>
-                        <div>
-                            Price: {this.props.product?.price}
+                        <div className="show-product-price-container">
+                            <div className="show-product-price-label">
+                                Price: 
+                            </div>
+                            <div className="show-product-price">
+                                ${this.props.product?.price}
+                            </div>
+                            
                         </div>
                         <h2>About this item</h2>
                         {this.props.product?.description}
                     </div>
                     <div className="show-product-cart">
-                        <div>{this.props.product?.price}</div>
-                        <div>Free Delivery:</div>
-                        <div>Fastest Delivery:</div>
-                        <div>Qty</div>
-                        <button className="button3">Add to Cart</button>
-                        <div>Secure Transaction</div>
-                        <div>Ships from</div>
-                        <div>Sold by</div>
+                        <div className="show-price">
+                            ${this.props.product?.price}
+                        </div>
+                        <div className="show-delivery-free">
+                            Free Delivery:
+                        </div>
+                        <div className="show-delivery-fast">
+                            Fastest Delivery:
+                        </div>
+                        <div className="show-stock">
+                            In Stock.
+                        </div>
+                        <div>
+                            <form action="">
+
+                            </form>
+                            <label htmlFor="">
+                                Qty
+                                <select name="qty" id="qty" value="qty">
+                                    <option value="1">1</option>
+                                    <option value="2">2</option>
+                                    <option value="3">3</option>
+                                    <option value="4">4</option>
+                                    <option value="5">5</option>
+                                    <option value="6">6</option>
+                                    <option value="7">7</option>
+                                    <option value="8">8</option>
+                                </select>
+                            </label>
+                            <button className="button3">Buy Now</button>
+                            <button className="button4">Add to Cart</button>
+                        </div>
+                        <div className="secure-transaction">
+                            <div className="secure-transaction-icon">
+                                <FaLock />
+                            </div>
+                            <div className="secure-transaction-text">
+                                Secure Transaction
+                            </div>
+                        </div>
+                        <div className="show-ship-container">
+                            <div>
+                                Ships from
+                            </div>
+                            <div className="show-jamazon">
+                                Jamazon.com
+                            </div>
+                        </div>
+                        <div className="show-sold-container">
+                            <div>
+                                Sold by
+                            </div>
+                            <div className="show-jamazon">
+                                Jamazon.com
+                            </div>
+                        </div>
                         <div>Return Policy</div>
                     </div>
                 </div>
