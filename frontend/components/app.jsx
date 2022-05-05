e303c8b1014a014f2ac89bcbba3651095f2c4062
Jackson <jacksondooley24@gmail.com>
2022-05-05 12:33:15 -0400

    refactored app

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -1,19 +1,41 @@
 import React from "react";
-import { Route, Switch } from "react-router-dom";
+import { Route, Switch, useLocation } from "react-router-dom";
 import LoginFormContainer from "./session/login_form_container";
 import SignupFormContainer from "./session/signup_form_container";
 import { AuthRoute } from "../util/route_util";
-import Main from "./main/main";
+import NavBar from "./navbar/nav_bar";
+import Footer from "./footer/footer";
+import Splash from "./splash/splash";
+import CartContainer from "./cart/cart_container";
+import ProductsContainer from "./product_index/products_container";
+import NewReviewContainer from "./reviews/new_review_container";
+import show_product_container from "./product_show/show_product_container";
 
 
-const App = () => (
-    <div>
+const App = () => {
+    const location = useLocation();
+
+    return (
+    <>
+        <header>
+            {location.pathname == '/login' || location.pathname == '/signup' ? null : <NavBar />}
+        </header>
         <Switch>
             <AuthRoute path="/login" component={LoginFormContainer} />
             <AuthRoute path="/signup" component={SignupFormContainer} />
-            <Route path="/" component={Main}/>
+            <Route path="/cart" component={CartContainer} />
+            <Route exact path="/products/:id/review/:crud" component={NewReviewContainer} />            
+            <Route path="/products/:id" component={show_product_container} />
+            <Route exact path="/products" component={ProductsContainer}/>
+            <Route path="/:category" component={ProductsContainer} />
+            <Route path="/cart" component={CartContainer} />
+            <Route path="/" exact component={Splash} />
         </Switch>
-    </div>
-)
+        <footer>
+        {location.pathname == '/login' || location.pathname == '/signup' ? null : <Footer />}
+        </footer>
+    </>
+    )
+}
 
 export default App;
